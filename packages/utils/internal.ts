a1f24680ca20e02e4d50e2b385288cdfc8b3d8d3
君惜 <xinlei3166@126.com>
2023-08-15 10:33:46 +0800

    feat: add logoutCleanup method

--- packages/utils/internal.ts
+++ packages/utils/internal.ts
@@ -1,5 +1,12 @@
 import Cookies from 'js-cookie'
 
+// @ts-ignore
+import router from '@/router'
+// @ts-ignore
+import { useUserStore } from '@/store/user'
+// @ts-ignore
+import { useMenuStore } from '@/store/menu'
+
 const storageKeyPrefix = import.meta.env.VITE_APP_STORAGE_KEY_PREFIX
 export const TokenKey = `${storageKeyPrefix}Token`
 
@@ -7,4 +14,14 @@ export const setToken = (token: string) => Cookies.set(TokenKey, token) // { exp
 export const getToken = () => Cookies.get(TokenKey)
 export const removeToken = () => {
   Cookies.remove(TokenKey)
+}
+
+// logout
+export const logoutCleanup = () => {
+  const userStore = useUserStore()
+  const menuStore = useMenuStore()
+  router.push('/login')
+  removeToken()
+  userStore.cleanup()
+  menuStore.cleanup()
 }
