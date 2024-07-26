7997207f071af78df1a887bb126351715678a950
君惜 <xinlei3166@126.com>
2024-07-26 15:50:27 +0800

    chore: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
   },
   plugins: ['@typescript-eslint'],
   extends: [
+    // 'eslint:recommended',
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'plugin:vue/vue3-recommended',
     'plugin:prettier/recommended' // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
