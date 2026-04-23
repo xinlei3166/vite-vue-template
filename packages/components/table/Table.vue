e85bbec3d8562592c46ccdcdfbaddcb9aa14a562
君惜 <xinlei3166@126.com>
2026-04-23 19:57:44 +0800

    fix: 修复 extraParams 类型定义，简化参数处理逻辑

--- packages/components/table/Table.vue
+++ packages/components/table/Table.vue
@@ -66,8 +66,8 @@ export default { inheritAttrs: false }
 
 <script lang="ts" setup>
 import type { TableProps, TableChangeData, SortInfo } from 'tdesign-vue-next'
-import type { PropType, CSSProperties, ComputedRef } from 'vue'
-import { ref, computed, unref, toRefs, onBeforeMount, useAttrs } from 'vue'
+import type { PropType, CSSProperties } from 'vue'
+import { ref, computed, toRefs, onBeforeMount, useAttrs } from 'vue'
 import { useData } from '@packages/hooks'
 import { deepClone } from '@packages/utils'
 import type { default as SearchType } from '../search/index.vue'
@@ -107,7 +107,7 @@ const props = defineProps({
 
   // method
   extraParams: {
-    type: Object as PropType<ComputedRef<Record<string, any>> | Record<string, any>>,
+    type: Object as PropType<Record<string, any>>,
     default: () => ({})
   },
   transformTableParams: {
@@ -151,6 +151,7 @@ const tableSlotNames = computed(() => {
 // props
 const {
   searchModel,
+  extraParams,
   pagination: _pagination,
   transformTableParams: _transformTableParams,
   useDataParams: _useDataParams
@@ -225,8 +226,7 @@ const tableParams = computed(() => {
 
 // useData
 const params = computed(() => {
-  const extraParams = unref(props.extraParams)
-  return { ...searchModel.value, ...extraParams, ...tableParams.value }
+  return { ...searchModel.value, ...extraParams.value, ...tableParams.value }
 })
 
 const useDataParams = computed(() => {
